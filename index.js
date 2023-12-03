8742764e83408cf08f82d560ebf45d52b17ba9dc
Eli-b-good <eli.i.henderson95@gmail.com>
2023-12-02 19:04:41 -0500

    UPDATE: roubust error catching

--- index.js
+++ index.js
@@ -32,8 +32,23 @@ app.post("/api/spotify/token", async (req, res) => {
     console.log({ data });
     res.json(data);
   } catch (error) {
-    console.log({ error: error.response.request });
-    res.status(400).json(error);
+    console.error("Error message:", error.message);
+
+    if (error.response) {
+      // The request was made and the server responded with a status code
+      // that falls out of the range of 2xx
+      console.error("Response data:", error.response.data);
+      console.error("Response status:", error.response.status);
+      console.error("Response headers:", error.response.headers);
+    } else if (error.request) {
+      // The request was made but no response was received
+      console.error("Request data:", error.request);
+    } else {
+      // Something happened in setting up the request that triggered an error
+      console.error("Error", error.message);
+    }
+
+    res.status(400).json({ message: error.message });
   }
 });
 
